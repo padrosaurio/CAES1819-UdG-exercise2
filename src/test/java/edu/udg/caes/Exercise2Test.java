e7f203c5973314c0b24c187f95cad4d71d6eb2e2
Albert <albertpadrosa97@gmail.com>
2018-10-14 12:21:05 +0200

    Testos incorrectes, a causa de la falta d'especificacio

--- src/test/java/edu/udg/caes/Exercise2Test.java
+++ src/test/java/edu/udg/caes/Exercise2Test.java
@@ -3,6 +3,8 @@ package edu.udg.caes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Time;
+import java.util.InputMismatchException;
 import java.util.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,6 +14,8 @@ public class Exercise2Test {
     Vector a = new Vector();
     Vector b = new Vector();
     Vector c = null;
+    Vector d = null;
+    Vector e = new Vector();
 
 
     @BeforeEach
@@ -21,7 +25,53 @@ public class Exercise2Test {
         a.add(2);
         b.add(2);
         b.add(3);
+        e.add(new Time(1,1,1));
+    }
 
+    //Ara com que hi ha repetits i la funció 'union' no posa elements repetits el següent test falla.
+    @Test
+    public void testUnionWithDuplicates()
+    {
+        Vector<Integer> expected = new Vector();
+        expected.add(1);
+        expected.add(2);
+        expected.add(2);
+        expected.add(3);
+
+        final Vector union = new Exercise2().union(a,b);
+
+        assertEquals(union, expected);
+    }
+
+    //El vector c és null, per tant, no té elements i el resultat de la funció hauria de retornar una llista buida
+    @Test
+    public void testUnionWithOneNullVector()
+    {
+        //Vector inicialitzat però vuit
+        Vector<Integer> expected = new Vector();
+
+        final Vector union = new Exercise2().union(c,b);
+
+        assertEquals(union, expected);
+    }
+
+    //El vector c i el d són null, per tant, no té elements i la funció hauria de retornar una llista buida
+    @Test
+    public void testUnionWithBothNullVector()
+    {
+        //Vector inicialitzat però vuit
+        Vector<Integer> expected = new Vector();
+
+        final Vector union = new Exercise2().union(c,d);
+
+        assertEquals(union, expected);
+    }
+
+    //Els vectors tenen el mateix tipus de dades.
+    @Test
+    public void unioDeDiferentsTipusTest()
+    {
+        assertThrows(InputMismatchException.class, () -> Exercise2.union(a,e));
     }
 
 
